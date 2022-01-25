dfd113915c82a05940631d3d6eeae5a56e98b604
Nuno Pato <nunopato@gmail.com>
2022-01-25 11:24:27 -0100

    Fix app endpoint

--- src/utils/nhost.ts
+++ src/utils/nhost.ts
@@ -2,7 +2,7 @@ import { NhostClient } from "@nhost/nhost-js";
 
 const nhost = new NhostClient({
   //backendUrl: "https://xtrzksvfzneoomchnxpa.nhost.run"
-  backendUrl: "http://localhost:1337",
+  backendUrl: "https://xdbhycnowcbphtatuttb.nhost.run",
 });
 
 export { nhost };
