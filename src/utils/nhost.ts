f9150ef27d4f137669cb2120c379f2b8abf0b8b5
Nuno Pato <nunopato@gmail.com>
2022-01-27 19:27:13 -0100

    Test london

--- src/utils/nhost.ts
+++ src/utils/nhost.ts
@@ -2,7 +2,7 @@ import { NhostClient } from "@nhost/nhost-js";
 
 const nhost = new NhostClient({
   //backendUrl: "https://xtrzksvfzneoomchnxpa.nhost.run"
-  backendUrl: "https://xdbhycnowcbphtatuttb.nhost.run",
+  backendUrl: "https://gxdynqkkmznbolvcepcy.nhost.run",
 });
 
 export { nhost };
