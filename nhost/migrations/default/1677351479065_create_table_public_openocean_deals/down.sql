225c0a45ef3c14cff7834fc7edf758c829d7f020
Nuno Pato <nunopato@gmail.com>
2023-03-03 12:47:56 -0100

    asd

--- nhost/migrations/default/1677351479065_create_table_public_openocean_deals/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS public.openocean_deals;
--- nhost/migrations/default/1677351479065_create_table_public_openocean_deals/up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE public.openocean_deals (uuid uuid DEFAULT gen_random_uuid() NOT NULL, company_name text NOT NULL, ticket_size numeric NOT NULL, PRIMARY KEY (uuid));
